4246d8cb426784777ade4121f557d37a44c7deed
Miguel José Borges Medina <121818423+borgesmj@users.noreply.github.com>
2023-10-02 15:10:53 -0500

    Update script.js

---
+++ Encontrar Numeros Primos/script.js
@@ -0,0 +1,34 @@
+function encontrarNumerosPrimos(inicio, fin) {
+	const numerosPrimos = []
+	const result = []
+	if (inicio < 1 || fin < 1) {
+		return numerosPrimos; // Devolver un array vacío si las entradas son inválidas
+	}
+
+	// Creamos un array de números desde el 2 hasta el fin del rango
+  // ya que el 2 es el numeroprimo mas pequeño 
+  // y asi podremos usar el metodo  Criba
+	for (let x = 2; x <= fin; x++) {
+		numerosPrimos.push(x);
+	}
+
+	// Evaluamos todos los numeros primos,
+	//utilizando el metodo  Criba de Eratóstenes
+	for (let index = 0; index < numerosPrimos.length; index++) {
+		for (let i = 0; i < numerosPrimos.length; i++) {
+			if (numerosPrimos[i] % numerosPrimos[index] === 0 && i !== index) {
+				numerosPrimos.splice(i, 1);
+			}
+		}
+	}
+
+	for (x = 0; x < numerosPrimos.length; x++) {
+		if (numerosPrimos[x] > inicio) {
+			result.push(numerosPrimos[x])
+			console.log(result)
+		}
+	}
+	return result
+}
+
+console.log(encontrarNumerosPrimos(10, 50)); // Debería mostrar [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
